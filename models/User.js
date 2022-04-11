d936cbe49a0bc9d792fc19f7c4b2a6784d68c136
adam0457 <adam0457@algonquinlive.com>
2022-04-11 10:48:03 -0400

    User model created

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const saltRounds = 14
+
+const schema = new mongoose.Schema({
+  firstName: {type: String, trim: true, maxlength: 64, required: true},
+  lastName: {type: String, trim: true, maxlength: 64,required: true },
+  email: {type: String, trim: true, maxlength: 512, required: true, unique:true},
+  password: {type: String, trim: true, maxlength: 70, required: true},
+  
+})
+
+schema.methods.generateAuthToken = function() {
+  const payload = {user: {_id: this._id}}
+  return jwt.sign(payload, 'superSecureSecret')
+}
+
+schema.statics.authenticate = async function(email, password) {
+  const user = await this.findOne({email: email})
+  const badHash = `$2b$${saltRounds}$invalidusernameaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`
+  const hashedPassword = user ? user.password : badHash
+  const passwordDidMatch = await bcrypt.compare(password, hashedPassword)
+
+  return passwordDidMatch ? user : null
+  
+}
+
+schema.pre('save', async function(next) {
+  // Only encrypt if the password property is being changed.
+  if (!this.isModified('password')) return next()
+
+  this.password = await bcrypt.hash(this.password, saltRounds)
+  next()
+})
+
+schema.methods.toJSON = function() {
+  const obj = this.toObject()
+  delete obj.password
+  delete obj.__v
+  return obj
+}
+
+const Model = mongoose.model('User', schema) // factory function returns a class
+
+
+module.exports = Model
